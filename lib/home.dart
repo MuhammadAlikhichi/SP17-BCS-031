748be56386fae54b33035043b1b2f1271616cbb9
muhammadali <muhammadalikhichi031@gmail.com>
2020-12-05 14:22:15 +0500

    commit 3 add two level

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
-class homepage extends StatelessWidget {
+class homepage extends StatefulWidget {
+  @override
+  _homepageState createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pinkAccent,
+        title: Text('Dice Game'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RaisedButton(
+              child: Text('simple Level'),
+
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              child: Text('Hard Level'),
+
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
